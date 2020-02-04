a1b04c43e4b349705fe33462e043e0e42e6586a2
Serena Bi <56049445+serena9604@users.noreply.github.com>
2020-02-03 20:46:51 -0800

    Add files via upload

---
+++ Fractions Assigmment/Fraction.java
@@ -0,0 +1,96 @@
+
+public class Fraction
+{
+    // instance variables - replace the example below with your own
+    private int n;
+    private int d;
+
+    public Fraction()
+    {
+        // initialise instance variables
+        this.n = 1;
+        this.d = 2;
+    }
+
+    public Fraction(int n, int d) {
+        this.n = n;
+        if (d == 0) {
+            System.out.println("Error");
+            d = 1;
+        } else {
+            this.d = d;
+        }
+    }
+
+    public Fraction(String f) {
+        int divideIndex = f.indexOf("/");
+        String num = f.substring (0, divideIndex);
+        String den = f.substring (divideIndex + 1);
+        if (den.equals("0")) {
+            System.out.println("Error");
+            den = "1";
+        }
+        this.n = Integer.parseInt(num);
+        this.d = Integer.parseInt(den);
+
+    }
+
+    public Fraction (Fraction f) {
+        this.n = f.n;
+        this.d = f.d;
+    };
+
+    public int getNum() {
+        return this.n;
+    }
+
+    public int getDemon() {
+        return this.d;
+    }
+
+    public String toString() {
+        return n + "/" + d;
+    }
+
+    public double toDouble() {
+        return n/d;
+    }
+
+    public int sampleMethod(int y)
+    {
+        return n/d;
+    }
+
+    public void reduce (Fraction f) {
+        GCF(n, d);
+    }
+
+    public void setNum(int n) {
+        this.n = n;
+    }
+
+    public void setDenom(int d) {
+        this.d = d;
+    }
+
+    public int GCF(int n, int d) {
+        while (n != d) {
+            if (n > d) {
+                n = n - d;
+            }
+            else if (d > n) {
+                d = d - n;
+            }
+        }
+        return n;
+    }
+
+    public static Fraction multiply (Fraction a, Fraction b) {
+        int numerator = a.n * b.n;
+        int denominator = a.d * b.d;
+        Fraction f = new Fraction(numerator, denominator);
+        f = reduce(f);
+        return f;
+
+    }
+}
---
+++ Fractions Assigmment/FractionDriver.java
@@ -0,0 +1,22 @@
+
+public class FractionDriver
+{
+    public static void main (String args[] ) {
+        Fraction f1 = new Fraction();
+        Fraction f12 = new Fraction();
+        Fraction f2 = new Fraction(3, 4);
+        Fraction f3 = new Fraction("5/6");
+        Fraction f4 = f2;
+        int x = f1.getNum();
+        int x2 = f2.getNum();
+        System.out.println("f1:" + f1);
+        System.out.println("f2:" + f2);
+        System.out.println("f3:" + f3);
+        System.out.println("f4:" + f4);
+
+        System.out.println(f3 + " x " + f4 + "=" + Fraction.multiply(f3, f4));
+        System.out.println();
+
+    }
+}
+
